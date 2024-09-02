fd172097f38d27071727c7cfae7e8f19ec885c89
Martin Carrasco <m.carrasco29@ufromail.cl>
2024-09-01 20:14:13 -0400

    Se realizan cambios a los aforos, y correcciones a permitir entrada, específicamente a la interacción entre filas y la gestión de aforo restante.

--- src/main/java/Gestion.java
+++ src/main/java/Gestion.java
@@ -15,31 +15,35 @@ public class Gestion {
     }
     public static int aforoDisponible (String [][] matrix, String room) {
         int people = 0;
+        int aforoVIP = 10;
+        int aforoGeneral = 5;
         if (Objects.equals(room, "VIP")){
             for (String[] strings : matrix) {
-                if ((Objects.equals(strings[2], room) && (Boolean.parseBoolean(strings[4])))) {
-                    people += (Integer.parseInt(strings[3]) + 1);
+                if (Objects.equals(strings[2], "VIP") && (Boolean.parseBoolean(strings[4]))) {
+                    people = people + (Integer.parseInt(strings[3]) + 1);
                 }
             }
-            return (20 - people);
-        } else {
+            return (aforoVIP - people);
+        } else if (Objects.equals(room, "General")){
             for (String[] strings : matrix) {
                 if (((Objects.equals(strings[2], room) && (Boolean.parseBoolean(strings[4]))))) {
                     people++;
                 }
             }
-            return (10 - people);
+            return (aforoGeneral - people);
+        } else {
+            return 0;
         }
     }
     public static String [][] ingresarPersona (String [][] matrix, int row) {
-        matrix[row-1][4] = "true";
+        matrix[(row-1)][4] = "true";
         return matrix;
     }
     public static boolean permitirEntrada (String [][] matrix, int row) {
-        if (Objects.equals(verificarBoleto(matrix, row-1), "VIP")) {
-            return  (verificarEdad(matrix, row-1) && validarInvitados(matrix, row-1) && (aforoDisponible(matrix, "VIP")+Integer.parseInt(matrix[row-1][3])+1 >= 20));
+        if (Objects.equals(verificarBoleto(matrix, row), "VIP")) {
+            return  (verificarEdad(matrix, (row)) && validarInvitados(matrix, row) && ((aforoDisponible(matrix, "VIP")+Integer.parseInt(matrix[(row-1)][3])+1) >= 10));
         } else if (Objects.equals(verificarBoleto(matrix,row), "General")){
-            return (verificarEdad(matrix, row-1) && validarInvitados(matrix, row-1) && (aforoDisponible(matrix, "General") > 0));
+            return (verificarEdad(matrix, row) && validarInvitados(matrix, row) && (aforoDisponible(matrix, "General") > 0));
         } else {
             return false;
         }
